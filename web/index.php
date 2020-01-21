137a4cd6b84324e8e122849a64467992494ba418
Handoyo <dyo.9913@gmail.com>
2020-01-21 14:44:45 +0700

    Update index.php
    add logic auto change in index

--- web/index.php
+++ web/index.php
@@ -1,5 +1,9 @@
 <?php
-    if (1==1) {
+    $date_target = strtotime("2020-01-25 00:00:00 GMT+7");
+    $utc_target = gmdate("Y-m-d H:i:s", $date_target);
+    $utc_now = gmdate("Y-m-d H:i:s");
+
+    if ($utc_now <= $utc_target) {
         include("countdown.php");
     } else {
         include("main.php");
