816a3b5994bc51ee04b4eca27763876dd9e31be1
Handoyo <dyo.9913@gmail.com>
2020-01-21 14:54:53 +0700

    Update index.php
    test index

--- web/index.php
+++ web/index.php
@@ -3,7 +3,7 @@
     $utc_target = gmdate("Y-m-d H:i:s", $date_target);
     $utc_now = gmdate("Y-m-d H:i:s");
 
-    if ($utc_now <= $utc_target) {
+    if ($utc_now >= $utc_target) {
         include("countdown.php");
     } else {
         include("main.php");
